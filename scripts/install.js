197f7dea8a4a541b2b21797206c7754d5e472a8f
Zachary Segal <zach@illproductions.com>
2020-06-30 23:04:50 -0700

    add a default .service file

--- scripts/install.js
+++ scripts/install.js
@@ -1,1 +1,13 @@
-console.log('no instructions yet')
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+
+// const install_path = path.join('/', 'opt', 'illpro', 'do-dns')
+// fs.mkdirSync(install_path, {'recursive': true})
+
+
+const service_file = path.join('.', 'scripts', 'do-dns.service')
+const services_dir = path.join('/', 'etc', 'systemd', 'system')
+fs.copyFileSync(service_file, path.join(services_dir, 'do-dns.service'))
+
